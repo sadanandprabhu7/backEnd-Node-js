99c5ac105cbe922afaba7d313023c112ca3a5c1b
sadanandprabhu7 <sadanandprabhu7@gmail.com>
2022-11-16 19:08:53 +0530

    url home about node pages

--- first-app.js
+++ first-app.js
@@ -1,8 +1,32 @@
  const http =require('http')
 
  const server = http.createServer((req, res)=>{
-
-    console.log('sadanand')
+   const url=req.url
+   if(url=='/home')
+   {
+      res.write('<html>')
+      res.write('<head><title> Welcome home</title></head>')
+      res.write('<body><h1> Welcome home</h1></body>')
+      res.write('</html>')
+      return res.end();
+   }
+   else if(url=='/about')
+   {
+      res.write('<html>')
+      res.write('<head><title>Welcome to About Us page</title></head>')
+      res.write('<body><h1>Welcome to About Us page</h1></body>')
+      res.write('</html>')
+      return res.end();
+   }
+   else if(url=='/node')
+   {
+      res.write('<html>')
+      res.write('<head><title>Welcome to my Node Js project</title></head>')
+      res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+      res.write('</html>')
+      return res.end();
+   }
+   
 
  })
 
