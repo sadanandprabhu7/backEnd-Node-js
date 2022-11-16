6cb171b3be95aae49d7e900643bcb9097c6007c9
sadanandprabhu7 <sadanandprabhu7@gmail.com>
2022-11-16 21:50:13 +0530

    reading and displayed on screen message

--- first-app.js
+++ first-app.js
@@ -1,8 +1,51 @@
- const http =require('http')
+ const http = require('http')
+ const fs =require('fs');
 
  const server = http.createServer((req, res)=>{
    const url=req.url
-   if(url=='/home')
+   const method=req.method;
+   const body=[];
+   if(url==='/')
+   {
+      
+      fs.readFile("message.text",{encoding: "utf-8"},(err,data)=>{
+         if(err){
+            console.log(err)
+         }
+         console.log(data);
+         //res.write('Content-Type','text/html')
+         res.write('<html>')
+         res.write('<head><title>Enter message</title></head>')
+         res.write(`<body>${data}</body>`)
+         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>')
+         res.write('</html>')
+         return res.end();
+      })
+   
+   }
+   else if(url==='/message' && method==='POST')
+   {
+     
+      
+      req.on('data',(chunk)=>{
+         console.log(chunk)
+         body.push(chunk)
+      })
+     return req.on('end',()=>{
+         const parseBody=Buffer.concat(body).toString();
+         
+        const message=parseBody.split('=')[1]
+        //res.write(`<h2>${message}</h2>`)
+        console.log(message);
+        fs.writeFile('message.text',message,err=>{
+         res.statusCode=302//for redirection
+         res.setHeader('Location','/')
+         return res.end()
+        })
+      })
+      
+   }
+   else if(url=='/home')
    {
       res.write('<html>')
       res.write('<head><title> Welcome home</title></head>')
